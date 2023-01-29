40809b5b59a4ae1ec94e09750a05cdb92d2c725a
Sam Fajobi <segzyfaj1@gmail.com>
2023-01-29 23:43:14 +0100

    Added Cache Update to Queries

--- src/components/ClientRow.js
+++ src/components/ClientRow.js
@@ -1,13 +1,25 @@
 import { useMutation } from "@apollo/client";
 import { DELETE_STAFF } from "../mutations/clientMutation";
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { GET_CLIENT } from "../queries/clientQueries";
+
 
 
 
 export const ClientRow = ({client}) => {
   const [ deleteStaff ] = useMutation(DELETE_STAFF, {
-    variables: { id: client.id}
-  });
+    variables: { id: client.id},
+
+    update(cache, {data: {deleteStaff}}) {
+      const {clients} = cache.readQuery({query: GET_CLIENT });
+      cache.writeQuery({
+        query: GET_CLIENT,
+        data: clients.filter(client => client.id !== deleteStaff.id)
+      })
+ 
+    }
+  });  
+
   console.log(client.id)
   return (
     <tr>
